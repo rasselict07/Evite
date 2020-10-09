f74755edbd955bbeb74eef92c350fb59e67f9a1e
Rezaul Karim <rasselict07@gmail.com>
2020-10-09 13:51:40 +0900

    event unit test

--- eviteapi/tests.py
+++ eviteapi/tests.py
@@ -1,3 +1,15 @@
+# Create your tests here.
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from eviteapi.models import Event
+
+
+class EventTestCase(TestCase):
+    def testPost(self):
+        event = Event(event_id="evt100001", name="Test Event", location="Tokyo", start_time=datetime.datetime.now(),
+                      end_time=datetime.datetime.now() + datetime.timedelta(days=1, hours=3))
+        self.assertEqual(event.event_id, "evt100001")
+        self.assertEqual(event.name, "Test Event")
+        self.assertEqual(event.location, "Tokyo")
